f938a8efe5db978d587b4d2e0e4ae98d24dec3f3
Mela Maluenda <maluenda.mela@gmail.com>
2023-11-27 00:31:25 +0100

    Add port for mongo conection

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-// Director de orquesta 🦥
+import chalk from "chalk";
+
+const port = process.env.PORT ?? 1914;
+
+if (!process.env.MONGODB_URL) {
+  chalk.red("Missing MongoDBconnection");
+  process.exit();
+}
