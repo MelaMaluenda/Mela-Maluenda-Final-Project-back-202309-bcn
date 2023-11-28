cf9d65a513ef25f9b663578cdaff8095ba41acf0
Mela Maluenda <maluenda.mela@gmail.com>
2023-11-28 13:04:49 +0100

    Add photos data interfaz in types

---
+++ src/features/photos/types.ts
@@ -0,0 +1,14 @@
+export type Category = "Long shot" | "Medium shot";
+
+export interface PhotosData {
+  _id: string;
+  title: string;
+  author: string;
+  year: string;
+  location: string;
+  publicSpace: string;
+  category: Category;
+  portfolioUrl: string;
+  whatMadeYouClick: string;
+  photoUrl: string;
+}
