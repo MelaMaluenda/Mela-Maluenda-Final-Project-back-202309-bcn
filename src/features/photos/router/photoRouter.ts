1f01efaa90c1eb4c5973be879e7cb2ce3241fb9a
Mela Maluenda <maluenda.mela@gmail.com>
2023-12-07 18:00:06 +0100

    Add in router add photo path

--- src/features/photos/router/photoRouter.ts
+++ src/features/photos/router/photoRouter.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import PhotosMongosooseRepository from "../repository/PhotosMongoseRepository.js";
 import PhotosController from "../controller/PhotosController.js";
+import photoValidation from "../schema/photoSchema.js";
 
 const photoRouter = Router();
 
@@ -10,5 +11,7 @@ const photoController = new PhotosController(photoRepository);
 photoRouter.get("/", photoController.getPhotos);
 
 photoRouter.delete("/:photoId", photoController.deletePhoto);
+
+photoRouter.post("/add", photoValidation, photoController.addPhoto);
 
 export default photoRouter;
